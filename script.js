6db2fff214022dd86115c9eec052d55bdb5a1a36
Bossolin <vladyslav.okipnyak@icloud.com>
2021-11-19 13:06:02 +0100

    added marker placement and change turn

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 const gameBoard = (() => {
-  const gameboard = [];
-
+  //render the board
+  const gameBoard = document.querySelector(".gameboard");
+  for (let i = 0; i < 9; i++) {
+    const cell = document.createElement("div");
+    cell.setAttribute("id", `${i}`);
+    gameBoard.appendChild(cell);
+  }
+  //store values on the board
+  const gameBoardArr = [];
+  //store win condish
+  //
   return {};
 })();
 
 const displayController = (() => {
+  //place markers
+  const cells = document.querySelectorAll(".gameboard div");
+  let turn = true;
+
+  const placeMarker = (e) => {
+    const cell = e.target;
+    cell.innerText = turn ? "X" : "O";
+    //change player
+    turn = !turn;
+  };
+
+  cells.forEach((cell) => {
+    cell.addEventListener("click", placeMarker, { once: true });
+  });
+
+  //check for win condish
+
   return {};
 })();
 
 const player = () => {
+  //ask for and store name
+  const name = prompt("Your name?");
+  //assign mark
+  const mark = "X";
+
   return {};
 };
